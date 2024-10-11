efc088ec0859bc2c8baf79f896973055d2eafb32
amao03 <amao@wustl.edu>
2024-10-11 14:11:58 -0500

    added vert args

--- Alice/color.py
+++ Alice/color.py
@@ -5,7 +5,7 @@ import moviepy.video.fx.all as vfx
 video_path_1 = "../../videos/butterfly-cropped.mp4"
 video_clip_1 = VideoFileClip(video_path_1)
 
-brightness_factor = 0.9  # Adjust as needed (higher values increase brightness)
+brightness_factor = 0.9  # Adjust as needed(higher values increase brightness)
 
 color_adjusted_clip = video_clip_1.fx(vfx.colorx, brightness_factor)
 final_clip = color_adjusted_clip.fx(vfx.lum_contrast, lum=150, contrast=0.3, contrast_thr=300)
@@ -13,7 +13,9 @@ final_clip = color_adjusted_clip.fx(vfx.lum_contrast, lum=150, contrast=0.3, con
 for frame in final_clip.iter_frames(fps=final_clip.fps):
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert from RGB to BGR
     cv2.imshow("Final Clip", frame)
-    
+    frame = cv2.normalize(
+        frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1
+    )
     # Check for the 'q' key press to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- Alice/object-direction.py
+++ Alice/object-direction.py
@@ -3,7 +3,7 @@ from collections import deque
 from imutils.video import VideoStream
 import numpy as np
 import argparse
-import cv2
+import cv2 # type: ignore
 import imutils
 import time
 
@@ -11,8 +11,11 @@ from moviepy.editor import VideoFileClip, ColorClip, CompositeVideoClip
 import moviepy.video.fx.all as vfx
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
+ap.add_argument("-vert", "--vertical", default=1, nargs='?',
+    help="video orientation")
 ap.add_argument("-v", "--video",
     help="path to the (optional) video file")
+
 ap.add_argument("-b", "--buffer", type=int, default=32,
     help="max buffer size")
 args = vars(ap.parse_args())
@@ -21,8 +24,10 @@ args = vars(ap.parse_args())
 # ball in the HSV color space
 greenLower = (5, 163, 53)
 greenUpper = (14,255,255)
-# initialize the list of tracked points, the frame counter,
-# and the coordinate deltas
+
+# greenLower = (10, 100, 20)
+# greenUpper = (25,255,255)
+
 pts = deque(maxlen=args["buffer"])
 
 laps = 0
@@ -31,76 +36,45 @@ counter = 0
 direction = ""
 prevDirection = ""
 laps = 0
-# if a video path was not supplied, grab the reference
-# to the webcam
+
 if not args.get("video", False):
     vs = VideoStream(src=0).start()
 # otherwise, grab a reference to the video file
 else:
     vs = cv2.VideoCapture(args["video"])
-    
-    # video_clip_1 = VideoFileClip(args["video"])
-    brightness_factor = 0.9  # Adjust as needed (higher values increase brightness)
 
-    # color_adjusted_clip = video_clip_1.fx(vfx.colorx, brightness_factor)
-    # final_clip = color_adjusted_clip.fx(vfx.lum_contrast, lum=150, contrast=0.3, contrast_thr=300)
-
-# allow the camera or video file to warm up
 time.sleep(2.0)
-# keep looping
+print(args["vertical"])
 while True:
-# for frame in final_clip.iter_frames(fps=final_clip.fps):
     frame = vs.read()
     frame = frame[1] if args.get("video", False) else frame
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
+    
+    frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+
     if frame is None:
         break
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
+
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    # grab the current frame
-    # frame = vs.read()
-    # handle the frame from VideoCapture or VideoStream
-    # frame = frame[1] if args.get("video", False) else frame
-    # # if we are viewing a video and we did not grab a frame,
-    # # then we have reached the end of the video
-    # if frame is None:
-    #     break
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    # frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
+
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
+
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = None
 
-
     # only proceed if at least one contour was found
     if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # only proceed if the radius meets a minimum size
+
         if radius > 0:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
             cv2.circle(frame, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
@@ -109,45 +83,31 @@ while True:
        
 # loop over the set of tracked points
     for i in np.arange(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
+
         if len(pts) < 11:
             continue
         if pts[i - 1] is None or pts[i] is None:
             continue
-        # check to see if enough points have been accumulated in
-        # the buffer
+
         if counter >= 10 and i == 1 and pts[-10] is not None:
-            # compute the difference between the x and y
-            # coordinates and re-initialize the direction
-            # text variables
+
             dX = pts[-10][0] - pts[i][0]
             dY = pts[-10][1] - pts[i][1]
             (dirX, dirY) = ("", "")
-            # ensure there is significant movement in the
-            # x-direction
-            if np.abs(dX) > 20:
+ 
+            if np.abs(dX) > 30 and args["vertical"]:
                 direction = "East" if np.sign(dX) == 1 else "West"
-            # ensure there is significant movement in the
-            # y-direction
-            # if np.abs(dY) > 20:
-            #     dirY = "North" if np.sign(dY) == 1 else "South"
-            # handle when both directions are non-empty
-            # if dirX != "" and dirY != "":
-            #     direction = "{}-{}".format(dirY, dirX)
-            # # otherwise, only one direction is non-empty
-            # else:
-            # direction = dirX if dirX != "" else dirY
+
+            if np.abs(dY) > 20 and not args["vertical"]:
+                direction = "North" if np.sign(dY) == 1 else "South"
+     
             if prevDirection != direction:
                 laps += 1
             prevDirection = direction
 
-    # otherwise, compute the thickness of the line and
-        # draw the connecting lines
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-    # show the movement deltas and the direction of movement on
-    # the frame
+
     cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
         0.65, (0, 0, 255), 3)
     cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
