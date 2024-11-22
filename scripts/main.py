c666bb1bb5bc896b59f0bd81598e5a5c2d45b873
amao03 <amao@wustl.edu>
2024-11-22 14:04:08 -0600

    narrowed down radius selection

--- scripts/main.py
+++ scripts/main.py
@@ -12,6 +12,8 @@ from warmer import *
 from videoPreprocessing import *
 from randomFrame import *
 
+import time
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-vert", "--vertical", default=1, nargs='?',
@@ -20,13 +22,15 @@ ap.add_argument("-v", "--video",
     help="path to the video file")
 ap.add_argument("-w", "--warmer", default=1, nargs='?',
     help="make video warmer")
+ap.add_argument("-c", "--crop", default=1, nargs='?',
+    help="crop video")
 ap.add_argument("-b", "--buffer", type=int, default=32,
     help="max buffer size")
 args = vars(ap.parse_args())
 
 
-greenLower = (2, 100, 100)
-greenUpper = (22,255,255)
+greenLower = (100, 150, 0)
+greenUpper = (140,255,255)
 
 # greenLower = (10, 100, 20)
 # greenUpper = (25,255,255)
@@ -40,38 +44,67 @@ direction = ""
 prevDirection = ""
 laps = 0
 
-if not args.get("video", False):
-    exit()
+# if not args.get("video", False):
+#     exit()
 
 videoName = args["video"]
 moviePyVideo = VideoFileClip(videoName+".mp4")
+
 cv2Video = cv2.VideoCapture(videoName+".mp4")
+# cap = cv2.VideoCapture("rtsp://admin :@192.168.1.117/Preview_01_main")
 
-randomFrame = getGoodRandomFrame(cv2Video, "Is this image good for cropping?")
 
-cropVideo(moviePyVideo, randomFrame, videoName)
-videoName = videoName + "-cropped"
-# now video is args["video"]-cropped
+if not args["crop"]:
+    randomFrame = getGoodRandomFrame(cv2Video, "Is this image good for cropping?")
+    cropVideo(moviePyVideo, randomFrame, videoName)
+    print("done crop")
+    videoName = videoName + "-cropped"
+    print(videoName)
+
 
 if not args["warmer"]:
     filter_video = warm_video(videoName+".mp4", (videoName+"-warmer.mp4"), 30)
     print("done watmer")
     videoName = videoName + "-warmer"
     print("captured")
+# vs = cv2.VideoCapture("rtsp://admin:@192.168.1.117/Preview_01_main")
+# vs = cv2.VideoCapture("rtsp://admin:cse454hik@192.168.1.119:554/Streaming/channels/101")
 
 vs = cv2.VideoCapture(videoName+".mp4")
-
+# vs = cv2.VideoCapture("rtsp://admin:@192.168.1.117/Preview_01_main")
 randomFilteredFrame = getGoodRandomFrame(vs, "Can you see the cap well?")
-
-greenLower, greenUpper = getHSVByUser(randomFilteredFrame)
+ret, frame = vs.read()
+greenLower, greenUpper = getHSVByUser(frame)
 print(greenLower, greenUpper)
 
 vs = cv2.VideoCapture(videoName+".mp4")
+# vs = cv2.VideoCapture("rtsp://admin:@192.168.1.117/Preview_01_main")
+# rtsp://admin:@192.168.1.117/Preview_01_main
 
-time.sleep(2.0)
+# time.sleep(2.0)
+
+start_time = time.time()
+prev_time = time.time()
+foundFirstPoint = None
+
+previousTime = 0
+elapsedTime = 0
+
+oldX = 0
+oldY = 0
+
+didStart = False
+
+splits = []
+
+# greenLower = (100, 150, 0)
+# greenUpper = (140,255,255)
+
+
 while True:
-    frame = vs.read()
-    frame = frame[1] if args.get("video", False) else frame
+    # frame = vs.read()
+    # frame = frame[1] if args.get("video", False) else frame
+    ret, frame = vs.read()
 
     if frame is None:
         break
@@ -88,7 +121,7 @@ while True:
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = None
-
+    
     # only proceed if at least one contour was found
     if len(cnts) > 0:
         c = max(cnts, key=cv2.contourArea)
@@ -96,13 +129,31 @@ while True:
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-        if radius > 0:
+        if radius > 10:
+            # print(radius)
             cv2.circle(frame, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
             pts.appendleft(center)
 
-       
+            if not foundFirstPoint:
+                foundFirstPoint = center
+                start_time = time.time()
+                
+
+            elapsedTime = time.time() - previousTime 
+
+            # print(elapsedTime)
+            if elapsedTime > 1:
+                displacement = int(x) - oldX
+                # print(oldX, x)
+                speed = np.abs(displacement) / elapsedTime
+                previousTime = time.time()
+                # print(speed)
+
+            oldX = int(x)
+            oldY = int(y)
+    
 # loop over the set of tracked points
     for i in np.arange(1, len(pts)):
 
@@ -112,19 +163,28 @@ while True:
             continue
 
         if counter >= 10 and i == 1 and pts[-10] is not None:
-
+            # print(pts)
             dX = pts[-10][0] - pts[i][0]
-            dY = pts[-10][1] - pts[i][1]
+            dY = pts[-10][0] - pts[i][0]
             (dirX, dirY) = ("", "")
- 
-            if np.abs(dX) > 30 and args["vertical"]:
+
+            
+            if np.abs(dX) > 75 and args["vertical"]:
                 direction = "East" if np.sign(dX) == 1 else "West"
 
             if np.abs(dY) > 20 and not args["vertical"]:
                 direction = "North" if np.sign(dY) == 1 else "South"
-     
+        
             if prevDirection != direction:
+                if laps == 0:
+                    start_time = 0
+                else:
+                    splits.append(time.time() - prev_time)
+                        # print(time.time() - prev_time)
+
                 laps += 1
+                prev_time = time.time()
+
             prevDirection = direction
 
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
@@ -137,6 +197,14 @@ while True:
         0.35, (0, 0, 255), 1)
     cv2.putText(frame, str(laps), (100, 30), cv2.FONT_HERSHEY_SIMPLEX,
         0.65, (0, 0, 255), 3)
+    # cv2.putText(frame, str(round(time.time() - 0,2)), (200, 30), cv2.FONT_HERSHEY_SIMPLEX,
+    #     0.65, (0, 0, 255), 3)
+    
+    startY = 30
+    for i in splits:
+        cv2.putText(frame, str(round(i,2)), (300, startY), cv2.FONT_HERSHEY_SIMPLEX,
+        0.45, (0, 0, 255), 1)
+        startY = startY + 20
         
     # show the frame to our screen and increment the frame counter
     cv2.imshow("Frame", frame)
